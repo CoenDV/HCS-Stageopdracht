2298720c227967731824facb249faed1eca0cc7b
CoenDV <c.oen@hotmail.nl>
2024-10-02 15:44:06 +0200

    Refactor llm_utils.py to improve code structure and readability. Containerfile for llm-api

--- llm_api/src/llm_utils.py
+++ llm_api/src/llm_utils.py
@@ -3,22 +3,33 @@ import json
 
 def generate_llm_response(prompt):
 
-    # Retrieve the documents relevant to the prompt
-    retrieved_docs = requests.post(
+    retrieved_docs = getRelevantDocuments(prompt)
+
+    data = createPayload(prompt, retrieved_docs)
+    print("Data: ", json.dumps(data, indent=4))
+
+    response = generateResponse(data)
+    
+    response.raise_for_status()
+
+    return response.json().get("choices")
+
+### Helper functions ###
+
+def getRelevantDocuments(prompt: str):
+    return requests.post(
         "https://milvus-api-v4-coen-de-vries-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/search/",
         json={"query": prompt}
     )
 
-    print(retrieved_docs.json())
-
+def createPayload(prompt: str, retrieved_docs: str):
+    
     # Concatenate the retrieved documents into a single string
-    if retrieved_docs.json() == [[]]:
-        context_text = "No relevant documents found."
-    else:
+    context_text = "No relevant documents found."
+    if retrieved_docs.json() != [[]]:
         context_text = "".join(f"{doc}" for doc in retrieved_docs.json()[0][0]['entity']['text'])
 
-    # Build the data payload
-    data = {
+    return {
         "messages": [
             {
                 "role": "system",
@@ -35,16 +46,8 @@ def generate_llm_response(prompt):
         "max_tokens": 100,
     }
 
-    print("Data: ", json.dumps(data, indent=4))
-
-    # Send the request to the LLM
-    response = requests.post(
-        "http://localhost:55760/v1/chat/completions",
+def generateResponse(data: str):
+    return requests.post(
+        "http://10.88.0.49:55760/v1/chat/completions",
         json=data
     )
-
-    print("Response: ", json.dumps(response.json().get("choices"), indent=4))
-    
-    response.raise_for_status()
-
-    return response.json().get("choices")
