6bff38289d77a6d437ed557ab78d747cccce27df
CoenDV <c.oen@hotmail.nl>
2024-10-09 15:16:54 +0200

    Fixed links

--- frontend/src/config/router.js
+++ frontend/src/config/router.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Login from '@/components/user/login.vue'
+import Login from '@/components/user/Login.vue'
 import Dashboard from '@/components/dashboard/Dashboard.vue'
 import AvailableDocuments from '@/components/documents/AvailableDocuments.vue'
-import account from '@/components/user/account.vue'
+import account from '@/components/user/Account.vue'
 
 const router = createRouter({
     history: createWebHistory("/"),
